52bd28ddb2720dd000f56895cbaa234a8ec2e216
sergannn <su.vasiliev@gmail.com>
2024-02-01 23:58:03 +0300

    Update main.py
    params

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/new_notification")
+async def root(text: str):
+    return {"message": text}
